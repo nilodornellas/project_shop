cf41b8422ba8573ec6c4868224208109595589fe
nilodornellas <iannilo01@gmail.com>
2022-02-09 23:14:58 -0300

    Aplicando a rotina de um provider depender do outro.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -8,11 +8,14 @@ import 'package:projeto_shop/models/product.dart';
 import 'package:projeto_shop/utils/constants.dart';
 
 class ProductList extends ChangeNotifier {
-  final List<Product> _items = [];
+  String _token;
+  List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
       _items.where((product) => product.isFavorite).toList();
+
+  ProductList(this._token, this._items);
 
   int get itemsCount {
     return _items.length;
@@ -20,8 +23,8 @@ class ProductList extends ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response =
-        await http.get(Uri.parse('${Constants.PRODUCT_BASE_URL}.json'));
+    final response = await http
+        .get(Uri.parse('${Constants.PRODUCT_BASE_URL}.json?auth=$_token'));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
