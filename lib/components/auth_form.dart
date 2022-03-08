76776edfa0c56ce8d21e92223c046aaf51c74374
nilodornellas <iannilo01@gmail.com>
2022-03-07 22:41:26 -0300

    Implementando a animação com o AnimatedContainer.

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -110,9 +110,11 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
         padding: const EdgeInsets.all(16),
-        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+        height: _isLogin() ? 310 : 400,
         width: deviceSize.width * 0.75,
         child: Form(
           key: _formKey,
